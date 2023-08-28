e18f1378ecb4377e524623b64ff557622c866986
preich-work <preich_work@yahoo.com>
2023-08-27 20:21:58 -0400

    Corrected code to display placeholders

--- js/script.js
+++ js/script.js
@@ -1,46 +1,28 @@
-//unordered list where player's guessed letters will appear
 const guessedLetters = document.querySelector(".guessed-letters");
-
-//The button with the text “Guess!” in it.
 const guessButton = document.querySelector(".guess");
-
-//The text input where the player will guess a letter.
 const inputLetter = document.querySelector(".letter");
-
-//The empty paragraph where the word in progress will appear.
-const wordInProgess = document.querySelector("word-in-progrss");
-
-
-//The paragraph where the remaining guesses will display.
-const numOfGuessesLeft = document.querySelector(".remaining");
-
-//The span inside the paragraph where the remaining guesses will display.
-const spanRemainingGuesses = document.querySelector(".remaining span");
-
-//The empty paragraph where messages will appear when the player guesses a letter.
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
-
-//The hidden button that will appear prompting the player to play again.
 const playAgain = document.querySelector(".play-again");
 
-//Magnolia is your starting word to test out the game until you fetch words from a hosted file in a later step.
 const word = "magnolia";
 
-//Create and name a function to update the paragraph’s innerText for the “words-in-progress” element with circle symbols 
-const wordBeingGuessed = function (word){
+// Display our symbols as placeholders for the chosen word's letters
+const wordBeingGuessed = function (word) {
     const wordBeingGuessedLetters = [];
-    for (const letter of word) {
+    for (const letter of word){
         console.log(letter);
         wordBeingGuessedLetters.push("●");
     }
-    wordInProgess.innerText = wordBeingGuessedLetters.join("");
+    wordInProgress.innerText = wordBeingGuessedLetters.join("");
 };
-
 wordBeingGuessed(word);
 
-guessButton.addEventListener("click", function(e){
+guessButton.addEventListener = ("click", function (e){
     e.preventDefault();
-    const chosenLetter = inputLetter.value;
-    console.log(chosenLetter);
+    const guess = inputLetter.value;
+    console.log(guess);
     inputLetter.value = "";
 });
